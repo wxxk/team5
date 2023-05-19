037d545104a77f3dd9801a684498adc9681558af
KOSA <KOSA@DESKTOP-B6GH6P7>
2023-05-19 15:30:59 +0900

    cart, order

--- team5/src/model/ICartDAO.java
+++ team5/src/model/ICartDAO.java
@@ -3,9 +3,9 @@ package model;
 import java.util.ArrayList;
 
 public interface ICartDAO {
-	ArrayList<CartVO> getAllCart(String userId);
-	int insertCart(CartVO vo); //상품등록
-	int updateCart(CartVO vo); //수량만 수정
-	int allDeleteCart(CartVO vo); //카트 전체삭제
-	int deleteCart(CartVO vo); //카트 부분삭제
+	public ArrayList<CartVO> getAllCart(String userId);
+	public int insertCart(CartVO vo); //상품등록
+	public int updateCart(CartVO vo); //수량만 수정
+	public int allDeleteCart(CartVO vo); //카트 전체삭제
+	public int deleteCart(CartVO vo); //카트 부분삭제
 }
--- team5/src/model/OrderDAO.java
+++ team5/src/model/OrderDAO.java
@@ -10,18 +10,19 @@ import main.DataSource;
 
 public class OrderDAO implements IOrderDAO {
 
+	@Override
 	public ArrayList<OrderVO> getAllOrderList(){
 		ArrayList<OrderVO> orderList = new ArrayList<OrderVO>();
-		
+
 		String sql = 
-			"SELECT o.order_id, u.user_name, u.user_phone_number, u.user_address,"
-			+ "p.product_img, p.product_name, pd.options, c.cart_cnt, p.product_price"	
-			 + "FROM users u "
-			 + "JOIN cart c ON u.user_id = c.user_id"
-			 + "JOIN orders o ON c.product_id = o.product_id"
-			 + "JOIN product p ON o.product_id = p.product_id"
-			 + "JOIN product_detail pd ON p.product_id = pd.product_id";
-		
+				"SELECT o.order_id, u.user_name, u.user_phone_number, u.user_address,"
+						+ "p.product_img, p.product_name, pd.options, c.cart_cnt, p.product_price"	
+						+ "FROM users u "
+						+ "JOIN cart c ON u.user_id = c.user_id"
+						+ "JOIN orders o ON c.product_id = o.product_id"
+						+ "JOIN product p ON o.product_id = p.product_id"
+						+ "JOIN product_detail pd ON p.product_id = pd.product_id";
+
 		Connection con = null;
 		try {
 			con = DataSource.getConnection();
@@ -38,6 +39,7 @@ public class OrderDAO implements IOrderDAO {
 				od.setOptions(rs.getString("product_name"));
 				od.setCartCnt(rs.getInt("cart_cnt"));	
 				od.setProductPrice(rs.getInt("product_price"));
+				orderList.add(od);
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -48,9 +50,11 @@ public class OrderDAO implements IOrderDAO {
 	}
 
 
+
 	@Override
-	public OrderVO getOrder(String userId) {
-		OrderVO vo = null;
+	public ArrayList<OrderVO> getOrderList(String userId){
+		ArrayList<OrderVO> orderList = new ArrayList<OrderVO>();
+
 		String sql = 
 				"SELECT o.order_id,p.product_name, c.cart_cnt, p.product_price, u.user_name, u.user_address, u.user_phone_number "
 						+"FROM users u "
@@ -58,30 +62,93 @@ public class OrderDAO implements IOrderDAO {
 						+"JOIN orders o ON c.product_id = o.product_id "
 						+"JOIN product p ON o.product_id = p.product_id "
 						+"WHERE u.user_id = ?";
-			Connection con = null;
-			PreparedStatement stmt = null;
-			try {
-				con = DataSource.getConnection();
-				stmt = con.prepareStatement(sql);
-				stmt.setString(1, userId);
-				ResultSet rs = stmt.executeQuery();
-				while(rs.next()) {
-					OrderVO od = new OrderVO();
-					od.setOrderId(rs.getInt("order_id"));
-					od.setProductName(rs.getString("product_name"));
-					od.setCartCnt(rs.getInt("cart_cnt"));	
-					od.setProductPrice(rs.getInt("product_price"));
-					od.setUserName(rs.getString("user_name"));
-					od.setUserAddress(rs.getString("user_address"));
-					od.setUserPhoneNumber(rs.getString("user_phone_number"));
-					
-					
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			} finally {
-				DataSource.closeConnection(con);
+		Connection con = null;
+		try {
+			con = DataSource.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				OrderVO od = new OrderVO();
+				od.setOrderId(rs.getInt("order_id"));
+				od.setProductName(rs.getString("product_name"));
+				od.setCartCnt(rs.getInt("cart_cnt"));	
+				od.setProductPrice(rs.getInt("product_price"));
+				od.setUserName(rs.getString("user_name"));
+				od.setUserAddress(rs.getString("user_address"));
+				od.setUserPhoneNumber(rs.getString("user_phone_number"));
+				orderList.add(od);
 			}
-			return vo;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			DataSource.closeConnection(con);
+		}
+		return orderList;
 	}
+
+
+
+	@Override
+	public int insertOrder(OrderVO vo) {
+		int count = 0;
+		String sql = "INSERT INTO orders (order_id, user_id, product_id, cart_id)"+
+				" VALUES (orders_seq.NEXTVAL, ?, ?, ?)";
+		Connection con = null;
+		try {
+			con = DataSource.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, vo.getOrderId());
+			stmt.setString(2, vo.getUserId());
+			stmt.setInt(3, vo.getProductId());
+			stmt.setInt(4, vo.getCartId());
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DataSource.closeConnection(con);
+		}
+		return count;
+	}
+
+	@Override
+	public int updateOrder(OrderVO vo) {
+		int count = 0;
+		String sql = "UPDATE orders SET user_address = ? AND options = ? WHERE order_id = ?";
+		Connection con = null;
+		try {
+			con = DataSource.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, vo.getUserAddress());
+			stmt.setString(2, vo.getOptions());
+			stmt.setInt(3, vo.getOrderId());
+			count = stmt.executeUpdate();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			DataSource.closeConnection(con);
+		}
+		return count;
+	}
+
+	@Override
+	public int deleteOrder(OrderVO vo) {
+		int deleteRow = 0;
+		String sql = "DELETE FROM orders WHERE order_id = ?";
+		Connection con = null;
+		try {
+			con=DataSource.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, vo.getOrderId());
+			deleteRow = stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			DataSource.closeConnection(con);
+		}
+		return deleteRow;
+	}
+
+
+
+
+
 }
